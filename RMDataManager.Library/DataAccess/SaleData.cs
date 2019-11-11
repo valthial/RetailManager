2fd8a4672cf9041b6949bc191081ef4ba85f1a32
Valentina Koronaiou <valthial@gmail.com>
2019-11-11 12:01:08 +0200

    Fix Transaction Exception Bug
    Transactions that were explicitly
    closed
    would throw an exception because
    the
    transaction was closed twice.

--- RMDataManager.Library/DataAccess/SaleData.cs
+++ RMDataManager.Library/DataAccess/SaleData.cs
@@ -82,13 +82,5 @@ namespace RMDataManager.Library.DataAccess
                 }
             }
         }
-        //public List<ProductModel> GetProducts()
-        //{
-        //    SqlDataAccess sql = new SqlDataAccess();
-
-        //    var output = sql.LoadData<ProductModel, dynamic>("dbo.spProduct_GetAll", new { }, "RMData");
-
-        //    return output;
-        //}
     }
 }
--- RMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
+++ RMDataManager.Library/Internal/DataAccess/SqlDataAccess.cs
@@ -52,6 +52,8 @@ namespace RMDataManager.Library.Internal.DataAccess
             _connection.Open();
 
             _transaction = _connection.BeginTransaction();
+
+            isClosed = false;
         }
         public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
         {
@@ -65,20 +67,38 @@ namespace RMDataManager.Library.Internal.DataAccess
             _connection.Execute(storedProcedure, parameters,
                 commandType: CommandType.StoredProcedure, transaction: _transaction);
         }
+        private bool isClosed = false;
         public void CommitTransaction()
         {
             _transaction?.Commit();
             _connection?.Close();
+
+            isClosed = true;
         }
         public void RollbackTransaction()
         {
             _transaction?.Rollback();
             _connection?.Close();
+
+            isClosed = true;
         }
 
         public void Dispose()
-        {
-            CommitTransaction();
+        { 
+            if (isClosed == false)
+            {
+                try
+                {
+                    CommitTransaction();
+                }
+                catch
+                {
+                    //TODO - Log this issue
+                }
+            }
+
+            _transaction = null;
+            _connection = null;
         }
     }
 }
