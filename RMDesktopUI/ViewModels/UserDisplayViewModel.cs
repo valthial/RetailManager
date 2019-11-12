b104fcb570e92edc3ede137a4f3780db9d81389e
Valentina Koronaiou <valthial@gmail.com>
2019-11-12 11:16:42 +0200

    Add Managing User Roles
    Adds user administration current roles
    and enables remove and add role buttons.

--- RMDesktopUI/ViewModels/UserDisplayViewModel.cs
+++ RMDesktopUI/ViewModels/UserDisplayViewModel.cs
@@ -19,7 +19,6 @@ namespace RMDesktopUI.ViewModels
         private readonly IUserEndpoint _userEndpoint;
 
         BindingList<UserModel> _users;
-
         public BindingList<UserModel> Users
         {
             get
@@ -33,8 +32,8 @@ namespace RMDesktopUI.ViewModels
             }
         }
 
+
         private UserModel _selectedUser;
-          
         public UserModel SelectedUser
         {
             get { return _selectedUser; }
@@ -42,12 +41,37 @@ namespace RMDesktopUI.ViewModels
             { 
                 _selectedUser = value;
                 SelectedUserName = value.Email;
+                UserRoles = new BindingList<string>(value.Roles.Select(x => x.Value).ToList());
+                LoadRoles();
                 NotifyOfPropertyChange(() => SelectedUser);
             }
         }
 
-        private string _selectedUserName;
+        private string _selectedUserRole;
+        public string SelectedUserRole
+        {
+            get { return _selectedUserRole; }
+            set 
+            { 
+                _selectedUserRole = value;
+                NotifyOfPropertyChange(() => SelectedUserRole);
+            }
+        }
 
+        private string _selectedAvailableRole;
+        public string SelectedAvailableRole
+        {
+            get { return _selectedAvailableRole; }
+            set 
+            {
+                _selectedAvailableRole = value;
+                NotifyOfPropertyChange(() => SelectedAvailableRole);
+            }
+        }
+
+
+
+        private string _selectedUserName;
         public string SelectedUserName
         {
             get 
@@ -58,6 +82,28 @@ namespace RMDesktopUI.ViewModels
             {
                 _selectedUserName = value;
                 NotifyOfPropertyChange(() => SelectedUserName);
+            }
+        }
+
+        private BindingList<string> _UserRoles = new BindingList<string>();
+        public BindingList<string> UserRoles
+        {
+            get { return _UserRoles; }
+            set 
+            {
+                _UserRoles = value;
+                NotifyOfPropertyChange(() => UserRoles);
+            }
+        }
+
+        private BindingList<string> _availableRoles = new BindingList<string>();
+        public BindingList<string> AvailableRoles
+        {
+            get { return _availableRoles; }
+            set
+            {
+                _availableRoles = value;
+                NotifyOfPropertyChange(() => AvailableRoles);
             }
         }
 
@@ -101,6 +147,36 @@ namespace RMDesktopUI.ViewModels
         {
             var userList = await _userEndpoint.GetAll();
             Users = new BindingList<UserModel>(userList);
+        }
+
+        private async Task LoadRoles()
+        {
+            var roles = await _userEndpoint.GetAllRoles();
+
+            foreach (var role in roles)
+            {
+                if (UserRoles.IndexOf(role.Value) < 0)
+                {
+                    AvailableRoles.Add(role.Value);
+                }
+            }
+        }
+
+        public async void AddSelectedRole()
+        {
+            await _userEndpoint.AddUserToRole(SelectedUser.Id, SelectedAvailableRole);
+
+            UserRoles.Add(SelectedAvailableRole);
+            AvailableRoles.Add(SelectedAvailableRole);
+        }
+
+        public async void RemoveSelectedRole()
+        {
+            await _userEndpoint.RemoveUserFromRole(SelectedUser.Id, SelectedUserRole);
+
+            AvailableRoles.Add(SelectedUserRole);
+            UserRoles.Remove(SelectedUserRole);
+           
         }
     }
 }
