bc617be35b494b131cf99d758348a2d6099d8e56
Valentina Koronaiou <valthial@gmail.com>
2019-11-12 14:26:43 +0200

    Added New API and Moved Over Functionality

---
+++ RMApi/Models/UserRolePairModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RMApi.Models
+{
+    public class UserRolePairModel
+    {
+        public string userId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
